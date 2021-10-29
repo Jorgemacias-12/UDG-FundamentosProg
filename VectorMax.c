f9b3f8bfd1ab5372852894e0d9c0273efc7a89ec
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-10-28 20:16:51 -0500

    Cambiando loop de aplicacion por etiqueta

--- VectorMax.c
+++ VectorMax.c
@@ -9,43 +9,50 @@ int mayor(int vector[], int cantidad, int maximo);
 int main(int argc, char **argv)
 {
     //----- VARIABLE DE REPETICIÓN -----
-    char bucle;
+    int option;
 
     //----- BUCLE DE REPETICION -----
-    do
+
+menu: //----- ENCABEZADO -----
+    puts("\n****************************************************************");
+    puts("*                 MAXIMO DE UN VECTOR DE ENTEROS               *");
+    puts("****************************************************************\n");
+
+    //----- VARIABLES -----
+    int vector[1000], cantidad, resultado, i = 0;
+
+    //----- INGRESAR LONGITUD DEL VECTOR -----
+    printf("\nINGRESA LA LONGITUD DEL VECTOR");
+    printf("\n==> ");
+    scanf("%d", &cantidad);
+
+    //----- FUNCION PARA PEDIR DATOS N VECES --------
+    for (int i = 0; i < cantidad; i++)
     {
-
-        //----- ENCABEZADO -----
-        puts("\n****************************************************************");
-        puts("*                 MAXIMO DE UN VECTOR DE ENTEROS               *");
-        puts("****************************************************************\n");
-
-        //----- VARIABLES -----
-        int vector[1000], cantidad, resultado, i = 0;
-
-        //----- INGRESAR LONGITUD DEL VECTOR -----
-        printf("\nINGRESA LA LONGITUD DEL VECTOR");
+        printf("\nINGRESA VALOR");
         printf("\n==> ");
-        scanf("%d", &cantidad);
+        scanf("%d", &vector[i]);
+    }
 
-        //----- FUNCION PARA PEDIR DATOS N VECES --------
-        for (int i = 0; i < cantidad; i++)
-        {
-            printf("\nINGRESA VALOR");
-            printf("\n==> ");
-            scanf("%d", &vector[i]);
-        }
+    //----- RESULTADO -----
+    resultado = mayor(vector, cantidad - 1, vector[0]);
+    printf("\nEL NUMERO MAYOR ES : %d", resultado);
 
-        //----- RESULTADO -----
-        resultado = mayor(vector, cantidad-1,vector[0]);
-        printf("\nEL NUMERO MAYOR ES : %d", resultado);
-
-        //----- CONFIRMAR O DENEGAR VUELTA -----
-        puts("\n\nDESEA CALCULAR OTROS NUMEROS : (S/N)");
-        printf("==> ");
-        scanf("%s", &bucle);
-        system("cls");
-    } while (bucle == 's' || bucle == 'S');
+    //----- CONFIRMAR O DENEGAR VUELTA -----
+    puts("\n\nDESEA CALCULAR OTROS NUMEROS : 0 = S, 1 = N");
+    printf("==> ");
+    scanf("%d", &option);
+    switch (option)
+    {
+        case 0:
+            system("cls");
+            goto menu;
+        case 1:
+            break;
+        default:
+            goto menu;
+    }
+    system("cls");
     getch();
     return 0;
 }
@@ -58,9 +65,7 @@ int mayor(int vector[], int cantidad, int maximo)
         return maximo;
     }
     else if (vector[cantidad] > maximo)
-    maximo = vector[cantidad];
+        maximo = vector[cantidad];
     return mayor(vector, cantidad - 1, maximo);
     return 0;
-
-    
 }
